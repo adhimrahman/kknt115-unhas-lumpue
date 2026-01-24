4965793aca61d295644573c4f18743b2cf9bf871
adhimrahman <adhimrahaman@gmail.com>
2026-01-24 18:41:44 +0800

    feat(app): add root layout with global styles, Poppins font, and site metadata

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Poppins } from 'next/font/google';
+import "./globals.css";
+
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+	title: "Kelurahan Lumpue Kota Parepare",
+	description: "Website resmi Kelurahan Lumpue Kota Parepare.",
+	robots: 'index, follow',
+	icons: {
+		icon: "/logo.svg"
+	}
+};
+
+export default function RootLayout({
+  	children,
+}: Readonly<{
+  	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={poppins.className}>
+				{children}
+			</body>
+		</html>
+	);
+}
