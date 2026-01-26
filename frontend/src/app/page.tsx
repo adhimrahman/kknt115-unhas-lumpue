52acf7840531487f979e1dcb91d1943879a517dc
adhimrahman <adhimrahaman@gmail.com>
2026-01-26 17:11:18 +0800

    feat(home): build homepage sections with navbar, hero, sambutan, stats, news, potensi, gallery, and footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,47 @@
+'use client';
+import { useRef } from 'react';
+import HeroSection from '@/components/HomePage/Hero';
+import Navbar from '@/components/layout/SiteNavbar';
+import SambutanLurah from '@/components/HomePage/SambutanLurah';
+import StatistikPetaWilayah from '@/components/HomePage/StatistikPetaWilayah';
+import BeritaPengumuman from '@/components/HomePage/BeritaPengumuman';
+import PotensiKelurahan from '@/components/HomePage/PotensiKelurahan';
+import GaleriPage from '@/components/HomePage/Galeri';
+import Footer from '@/components/layout/Footer';
+import KKNTributeSection from '@/components/HomePage/KKNTributeSection';
+import ScrollToTop from '@/components/ui/ScrollToTop';
+
 export default function HomePage() {
-	return (
-		<h1>Halaman Beranda</h1>
-	);
+    const sambutanRef = useRef<HTMLElement>(null);
+
+    const scrollToSambutan = () => {
+        if (sambutanRef.current) {
+            const y = sambutanRef.current.offsetTop;
+            window.scrollTo({ top: y - 80, behavior: 'smooth' });
+        }
+    };
+
+    return (
+        <div className="min-h-screen bg-white">
+            <Navbar />
+
+            <section className="hero w-full h-screen flex relative overflow-hidden">
+                <HeroSection onScrollToSambutan={() => {
+                    scrollToSambutan()
+                }} />
+            </section>
+
+            <section ref={sambutanRef}  className="py-20 bg-white">
+                <SambutanLurah />
+            </section>
+
+            <StatistikPetaWilayah />
+            <BeritaPengumuman />
+            <PotensiKelurahan />
+            <GaleriPage />
+            <KKNTributeSection />
+            <Footer />
+            <ScrollToTop />
+        </div>
+    );
 }
