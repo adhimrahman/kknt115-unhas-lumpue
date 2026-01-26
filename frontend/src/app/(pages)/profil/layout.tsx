b3efd7c429937fe9add265b002c0a0bf98d4c853
adhimrahman <adhimrahaman@gmail.com>
2026-01-26 17:12:20 +0800

    refactor(profil): simplify profil layout to rely on parent pages layout

--- frontend/src/app/(pages)/profil/layout.tsx
+++ frontend/src/app/(pages)/profil/layout.tsx
@@ -1,12 +1,7 @@
-import SiteNavbar from "@/components/layout/SiteNavbar";
-import Footer from "@/components/layout/Footer";
-
 export default function ProfilLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="min-h-screen flex flex-col">
-			<SiteNavbar />
-			<main className="grow">{children}</main>
-			<Footer />
+		<div className="pt-16">
+			{children}
 		</div>
 	);
 }
