36e82fae2f8755cc8b8a2b6df3243e3d99a2580d
adhimrahman <adhimrahaman@gmail.com>
2026-01-28 21:35:18 +0800

    refactor(profil): use ProfilBadge component on potensi page

--- frontend/src/app/(pages)/profil/potensi/page.tsx
+++ frontend/src/app/(pages)/profil/potensi/page.tsx
@@ -1,5 +1,13 @@
-import PotensiBadges from '@/components/sections/PotensiBadge';
+import PotensiBadgesProfil from '@/components/sections/ProfilBadge';
 
 export default function PotensiPage() {
-	return <PotensiBadges />;
+	return (
+		<section className="py-2">
+			<div className="">
+				<div className="bg-white rounded-2xl shadow-sm border border-slate-100 p-5 sm:p-7 lg:p-10">
+					<PotensiBadgesProfil />
+				</div>
+			</div>
+		</section>
+	)
 }
