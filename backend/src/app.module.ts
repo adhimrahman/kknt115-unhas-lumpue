3c4f1a556dfb3ca7d11af8782cf75e787e01ddc1
adhimrahman <adhimrahaman@gmail.com>
2026-01-29 05:16:27 +0800

    feat(backend): register ProfilModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfilModule } from './profil/profil.module';
 
 @Module({
-	imports: [],
+	imports: [ProfilModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
